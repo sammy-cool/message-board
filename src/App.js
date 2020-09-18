7019347e908d339adbbd9cbf918e2dd5c796e462
priyanshu-alt <priyanshu.alt191@gmail.com>
2020-09-18 15:42:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import Messages from './component/Messages'
+
+function App() {
+    return (
+        <div>
+            <h1>Message Board</h1>
+            <Messages />
+        </div>
+    )
+}
+
+export default App
